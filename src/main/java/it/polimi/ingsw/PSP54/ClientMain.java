f6be2efdfe06afc36f6974f2a539766f322bf786
AlessandroBatta <10524334@polimi.it>
2020-06-23 20:39:41 +0200

    bug fixed

--- src/main/java/it/polimi/ingsw/PSP54/ClientMain.java
+++ src/main/java/it/polimi/ingsw/PSP54/ClientMain.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 public class ClientMain {
     public static void main(String[] args) {
         Client client = new Client(12345);
+        try {
             client.startClient();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
